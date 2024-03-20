a959ff00c9770b42a8e86b69b92186e64ab11937
박재홍 <qkrkwl6666@gmail.com>
2024-03-20 19:25:39 +0900

    에러 수정

--- TileEditor.cpp
+++ TileEditor.cpp
@@ -20,3 +20,65 @@ void TileEditor::Init()
 	//tilemapSheet->SetPosition({ 50,50 });
 	//AddGo(tilemapSheet, Layers::Ui);
 }
+
+void TileEditor::Release()
+{
+}
+
+void TileEditor::Reset()
+{
+}
+
+void TileEditor::Update(float dt)
+{
+}
+
+void TileEditor::LateUpdate(float dt)
+{
+}
+
+void TileEditor::SetCheck(bool c)
+{
+}
+
+void TileEditor::Draw(sf::RenderWindow& window)
+{
+}
+
+void TileEditor::HandleMouseSelection()
+{
+}
+
+void TileEditor::TileMouseSelection(bool isRemove)
+{
+}
+
+void TileEditor::TileTypeMouseSelection()
+{
+}
+
+void TileEditor::TileSetTexture(const std::string& filePath)
+{
+}
+
+void TileEditor::typeTextSet()
+{
+}
+
+void TileEditor::typeTextDelete()
+{
+}
+
+std::wstring TileEditor::OpenFile(const wchar_t* filter, HWND owner)
+{
+	return std::wstring();
+}
+
+void TileEditor::SetActiveTypeUI(bool active)
+{
+}
+
+std::string TileEditor::convertToRelativePath(const std::wstring& absolutePathW)
+{
+	return std::string();
+}
